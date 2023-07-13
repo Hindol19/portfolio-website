8658f399671cda925b9cedc05aa8f5b5132b17ec
Hindol19 <hindolbanerjee5@gmail.com>
2023-07-14 01:58:32 +0530

    Added Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import "./Footer.scss";
 const Footer = () => {
-  return <div>I am Footer</div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-layout">
+        <div className="email">hindolbanerjee5@gmail.com</div>
+        <div className="link">www.hinu.com</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,24 +3,35 @@ import pic from "../../assets/Images/Removed.png";
 // import Links from "../Links/Links";
 import Project from "./Project/Project";
 import ButtonNew from "../../subcomponents/ButtonNew";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import CallReceivedIcon from "@mui/icons-material/CallReceived";
 import "./Home.scss";
+import { useMediaQuery } from "@mui/material";
 const Home = () => {
+  // const isDesktop = useMediaQuery("min-width:")
   return (
     <div className="home-container ">
       <div className="home-layout">
         <div className="my-image">
-          <img src={pic} alt="" />
+          <img className="image-mobile" src={pic} alt="" />
         </div>
         <div className="my-name">I am Hindol Banerjee</div>
         <div className="my-desc">Freelance Web-Designier and Developer</div>
       </div>
 
       <div className="about-section">
-        <div className="about-button">Know More</div>
+        <div className="about-section-layout">
+          <div className="about-button">Know More</div>
+
+          <h2 className="about-content">
+            <ArrowBackIcon />
+            More About Me
+          </h2>
+        </div>
       </div>
       <div className="projects-layout">
+        <h2 className="projects-title">My Works</h2>
         <div className="projects-section">
-          <Project />
           <Project />
           <Project />
           <Project />
